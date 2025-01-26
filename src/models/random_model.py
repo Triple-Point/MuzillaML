3959865c407a9c7484ef1f7e823544f8d05c5c2f
Richard Kilgour <richardkilgour@gmail.com>
2025-01-26 21:27:30 +0100

    Select artists at random

---
+++ src/models/random_model.py
@@ -0,0 +1,21 @@
+import logging
+from random import randrange
+from src.models.recommender_model import RecommenderModel
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class RandomModel(RecommenderModel):
+    def __init__(self, data):
+        super().__init__(data)
+        # TODO: Get the number of artists
+        self.num_artists = self.data.size(1)
+
+
+    def recommend_items(self, user, topn=10):
+        # Recommend the most popular items that the user hasn't seen yet at random
+        # Return a list of size topn chosen at random from the range
+
+        return [randrange(self.num_artists) for _ in range(topn)  ]
