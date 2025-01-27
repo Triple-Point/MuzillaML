dc27fd4465f522ffc82980508a1f25f1aafdeb04
Richard (Laptop) <richardkilgour@gmail.com>
2025-01-27 17:58:12 +0100

    Updated TFIDF model to new interface. It's not tested, but. Do not use!

--- src/models/tfidf_model.py
+++ src/models/tfidf_model.py
@@ -1,4 +1,5 @@
 import joblib
+import torch
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics.pairwise import linear_kernel
 
@@ -15,8 +16,7 @@ class TFIDFModel(RecommenderModel):
         joblib.dump(self.tfidf_matrix, save_file)
         self.tfidf_matrix = joblib.load(save_file)
 
-
-    def get_similar_user_index(self, user_id):
-        cosine_similarities = linear_kernel(self.tfidf_matrix[user_id], self.tfidf_matrix).flatten()
-        related_docs_indices = cosine_similarities.argsort()[:-10:-1]
+    def recommend_items(self, user: torch.sparse_coo_tensor, topn=10):
+        cosine_similarities = linear_kernel(self.tfidf_matrix[user], self.tfidf_matrix).flatten()
+        related_docs_indices = cosine_similarities.argsort()[:-topn:-1]
         return self.data['user_id'].unique()[related_docs_indices]
