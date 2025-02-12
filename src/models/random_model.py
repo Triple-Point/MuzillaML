487faafd331eb14c8b11d886a101ead390f64344
Richard (Laptop) <richardkilgour@gmail.com>
2025-02-12 21:17:08 +0100

    All init is done by the parent

--- src/models/random_model.py
+++ src/models/random_model.py
@@ -8,12 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 class RandomModel(RecommenderModel):
-    def __init__(self, data, user_id_to_index_map, artist_id_to_index_map):
-        super().__init__(data, user_id_to_index_map, artist_id_to_index_map)
-        # Get the number of artists
-        self.num_artists = self.data.size(1)
-
-    def recommend_items(self, artist_ids, topn=10):
+     def recommend_items(self, artist_ids, topn=10):
         # Recommend the most popular items that the user hasn't seen yet at random
         # Return a list of size topn chosen at random from the range
         return [randrange(self.num_artists) for _ in range(topn)]
