dc72725492564271e4cfb592b6a05d8111c02874
Richard (Laptop) <richardkilgour@gmail.com>
2025-02-13 11:43:44 +0100

    Minor cleanup

--- src/models/random_model.py
+++ src/models/random_model.py
@@ -2,13 +2,9 @@ import logging
 from random import randrange
 from src.models.recommender_model import RecommenderModel
 
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
 
 class RandomModel(RecommenderModel):
-     def recommend_items(self, artist_ids, topn=10):
+    def recommend_items(self, artist_ids, topn=10):
         # Recommend the most popular items that the user hasn't seen yet at random
         # Return a list of size topn chosen at random from the range
         return [randrange(self.num_artists) for _ in range(topn)]
