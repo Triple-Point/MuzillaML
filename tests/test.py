748c66ce5be5a5bc5af891034c4022194e1d0925
Richard (Laptop) <richardkilgour@gmail.com>
2025-02-13 13:22:48 +0100

    Tests are working again - and already found a bug :)

--- tests/test.py
+++ tests/test.py
@@ -1,20 +1,19 @@
 from scipy.sparse import csr_matrix
 
-from src.data.DataUtils import csr_to_torch_sparse_tensor
+from src.data.TensorUtils import dense_to_sparse
 from src.models.cosine_model import CosineModel
 
 # 2 users with 6 artists
-test_data = csr_to_torch_sparse_tensor(
-    csr_matrix([[0, 1, 2, 0, 3, 4],
-                [4, 0, 3, 2, 1, 0]]))
+test_data = dense_to_sparse([[0, 1, 2, 0, 3, 4],
+                             [4, 0, 3, 2, 1, 0]])
 model = CosineModel(test_data)
 
 # Same user#0, but missing artist #2
 user = [0, 1, 0, 0, 3, 4]
-new_artist = model.recommend_items(csr_to_torch_sparse_tensor(csr_matrix(user)))
+new_artist = model.recommend_items(dense_to_sparse([user]), topn=1)
 print(new_artist)
 
 # Similar to user#1, but missing artist #3
 user = [4, 0, 3, 0, 1, 1]
-new_artist = model.recommend_items(csr_to_torch_sparse_tensor(csr_matrix(user)))
+new_artist = model.recommend_items(dense_to_sparse([user]), topn=1)
 print(new_artist)
